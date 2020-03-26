d819cbb6dd85e0b23989358f007b6edf8d2cdfd0
DongHunWon <37091640+DongHunWon@users.noreply.github.com>
2020-03-26 18:55:05 +0900

    01 수업 과제

---
+++ 01/app.js
@@ -0,0 +1,19 @@
+console.log('app.js');
+
+var name = 'won';
+var Name = 'won';
+
+var bool = true;
+var month = ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'];
+
+var user = {
+    height : 170,
+    weight : 60,
+    footsize : 155,
+};
+
+console.log(user);
+
+console.log(0 == '');
+
+console.log(typeof (1 + '4'));
